169706a06face010bb7edae8be7ffb506cb5196c
Takayuki Goto <tak.yuki@gmail.com>
2013-12-03 16:53:58 -0600

    moved global variables

--- src/clj/fretset/css/util.clj
+++ src/clj/fretset/css/util.clj
@@ -1,12 +1,6 @@
 (ns fretset.css.util
   (:require [garden.core :refer [css]]))
 
-(def black "#0b0f11")
-(def gray "#afafaf")
-
-(def body-background "url('/images/blue_dark_background.jpg')")
-(def header-background "url('/images/bg-header.png')")
-
 (def clearfix
   ["&"
    {:*zoom 1}
